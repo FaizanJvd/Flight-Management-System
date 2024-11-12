190e1f933e49bbb9a39d17e5f2baf2ecdb92115d
faizanjvd <96561345+FaizanJvd@users.noreply.github.com>
2024-11-12 12:59:57 +0500

    feat:added the redis client

--- Server/middlewares/auth.js
+++ Server/middlewares/auth.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const { getUserById } = require('../services/authServices');
 
 const protect = async (req, res, next) => {
   let token;
@@ -7,7 +7,8 @@ const protect = async (req, res, next) => {
     try {
       token = req.headers.authorization.split(' ')[1];
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      req.user = await User.findById(decoded.id).select('-password');
+      let user = await getUserById(decoded.id);
+      req.user = user;
       next();
     } catch (error) {
       res.status(401).json({ message: 'Not authorized, token failed' });
